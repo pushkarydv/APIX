8e50e5de03100a5834bc77e5bee2bda32fc69f02
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-06-03 22:38:57 +0530

    meta tags

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,41 @@ export default function Home() {
       <Head>
         <meta charset="UTF-8" />
         <title>APIX</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="title" content="APIX" />
+        <meta
+          name="description"
+          content="APIX provides a list and docs to some free api's, so that you can build your dream projects sooner"
+        />
+        <meta
+          name="keywords"
+          content="api, apix, code, json, list , docs, application programming interface"
+        />{" "}
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />{" "}
+        <meta name="language" content="English" />{" "}
+        <meta name="author" content="Pushkar Yadav" />{" "}
+        <meta name="viewport" content="width=device-width,initial-scale=1" />{" "}
+        <meta property="og:type" content="website" />{" "}
+        <meta property="og:url" content="https://apix.pushkaryadav.in" />{" "}
+        <meta property="og:title" content="APIX" />{" "}
+        <meta
+          property="og:description"
+          content="APIX provides a list and docs to some free api's, so that you can build your dream projects sooner"
+        />{" "}
+        <meta
+          property="og:image"
+          content="https://apix.pushkaryadav.in/apix_banner.png"
+        />{" "}
+        <meta property="twitter:card" content="summary_large_image" />{" "}
+        <meta property="twitter:url" content="https://apix.pushkaryadav.in" />
+        <meta property="twitter:title" content="APIX" />
+        <meta
+          property="twitter:description"
+          content="APIX provides a list and docs to some free api's, so that you can build your dream projects sooner"
+        />
+        <meta
+          property="twitter:image"
+          content="https://apix.pushkaryadav.in/apix_banner.png"
+        />
       </Head>
       <HomePage />
     </div>
