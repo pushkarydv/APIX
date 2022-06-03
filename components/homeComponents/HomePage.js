c0e3d2bca98571ba21464572f9c1095706efa8dd
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-06-03 21:24:29 +0530

    tailwind and some components

---
+++ components/homeComponents/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "../nav/Nav";
+function HomePage() {
+  return (
+    <>
+      <Nav />
+    </>
+  );
+}
+
+export default HomePage;
---
+++ components/nav/Nav.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Link from "next/link";
+// import { useRouter } from "next/router";
+
+function Nav() {
+  //   const router = useRouter();
+  return (
+    <div className="text-3xl p-2 flex justify-between items-center">
+      <Link href="/">
+        <a className="text-slate-700">APIX</a>
+      </Link>
+      <div className="flex gap-4 justify-between items-center text-2xl">
+        {[
+          ["/", "Home"],
+          ["/expolre", "Explore"],
+          ["/about", "About"],
+        ].map(([url, name]) => (
+          <>
+            <Link href={url} key={name}>
+              <a>{name} </a>
+            </Link>
+          </>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
