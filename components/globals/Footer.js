3a94ddead8d83771b8d45a6285788af3fee8cf07
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-06-04 17:31:16 +0530

    Create Footer.js

---
+++ components/globals/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <div className="text-center text-slate-400 text-2xl px-2">
+        &lt;/&gt; &amp; crafted with 💜 by{" "}
+        <a
+          href="https://github.com/pushkarydv"
+          className="no-underline text-slate-500"
+        >
+          Pushkar Yadav
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
