813f2903c53372f52d8a04cc7f13048f8deee780
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-06-04 17:31:27 +0530

    Update HomePage.js

--- components/homeComponents/HomePage.js
+++ components/homeComponents/HomePage.js
@@ -1,29 +1,13 @@
 import React from "react";
-import Nav from "../nav/Nav";
-import Link from "next/link";
-import Map from "../../public/assets/map.svg";
+import Nav from "../globals/Nav";
+import Footer from "../globals/Footer";
+import Hero from "./Hero";
 function HomePage() {
   return (
     <>
       <Nav />
-      <div className="home min-h-screen flex flex-col justify-center gap-12 items-center">
-        <div className="text-center m-2 text-6xl font-medium text-slate-600">
-          Build Next Gen Apps with these Api{"'"}s
-        </div>
-        <Link href="/explore">
-          <a className="transition before-box py-2 rounded-2xl bg-purple-700 text-slate-200 px-6 text-4xl  outline outline-4 outline-purple-400 hover:bg-purple-600 focus:scale-90">
-            Explore
-          </a>
-        </Link>
-      </div>
-      <style jsx>
-        {`
-          .home {
-            background: url("/assets/map.svg") center center fixed no-repeat;
-            background-size: contain;
-          }
-        `}
-      </style>
+      <Hero />
+      <Footer />
     </>
   );
 }
