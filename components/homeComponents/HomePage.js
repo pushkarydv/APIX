6bf4783ae085357496dbbe5a97b9ced02d4850ad
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-06-04 10:24:15 +0530

    background and hero

--- components/homeComponents/HomePage.js
+++ components/homeComponents/HomePage.js
@@ -5,6 +5,19 @@ function HomePage() {
   return (
     <>
       <Nav />
+      <div className="home min-h-screen flex justify-center items-center">
+        <div className="text-center m-2 text-8xl font-medium text-purple-600">
+          Build Next Gen Apps with these Api{"'"}s
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .home {
+            background: url("/assets/map.svg") center center fixed no-repeat;
+            background-size: contain;
+          }
+        `}
+      </style>
     </>
   );
 }
