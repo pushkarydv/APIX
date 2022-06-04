dd5efea8a558a1f8b51908eb36b88f4af24796bd
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-06-04 09:59:23 +0530

    Update Nav.js

--- components/nav/Nav.js
+++ components/nav/Nav.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Link from "next/link";
-// import { useRouter } from "next/router";
+import { useRouter } from "next/router";
 
 function Nav() {
-  //   const router = useRouter();
+  const router = useRouter();
   return (
-    <div className="text-3xl p-2 flex justify-between items-center">
+    <div className="text-3xl flex justify-between items-center">
       <Link href="/">
-        <a className="text-slate-700">APIX</a>
+        <a className="text-slate-700 p-2">APIX</a>
       </Link>
       <div className="flex gap-4 justify-between items-center text-2xl">
         {[
@@ -15,7 +15,14 @@ function Nav() {
           ["/explore", "Explore"],
           ["/about", "About"],
         ].map(([url, name]) => (
-          <div key={name}>
+          <div
+            key={name}
+            className={
+              router.asPath === url
+                ? "bg-slate-800 text-slate-100 p-2"
+                : " text-slate-800 p-2"
+            }
+          >
             <Link href={url}>
               <a>{name}</a>
             </Link>
