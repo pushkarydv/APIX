1474fe8494e283d3547d9b10ab2e9596122fc846
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-06-11 12:19:08 +0530

    aos animation

--- components/homeComponents/Hero.js
+++ components/homeComponents/Hero.js
@@ -1,15 +1,22 @@
 import React from "react";
 import Link from "next/link";
-import Map from "../../public/assets/map.svg";
 function Hero() {
   return (
     <>
       <div className="home min-h-screen flex flex-col justify-center gap-12 items-center">
-        <div className="text-center m-2 text-6xl font-medium text-slate-600">
+        <div
+          className="text-center m-2 text-6xl font-medium text-slate-600"
+          data-aos="zoom-out"
+          data-aos-duration="600"
+        >
           Build Next Gen Apps with these Api{"'"}s
         </div>
         <Link href="/explore">
-          <a className="transition before-box py-2 rounded-2xl bg-purple-700 text-slate-200 px-6 text-4xl  outline outline-4 outline-purple-400 hover:bg-purple-600 focus:scale-90">
+          <a
+            className="transition before-box py-2 rounded-2xl bg-purple-700 text-slate-200 px-6 text-4xl  outline outline-4 outline-purple-400 hover:bg-purple-600 focus:scale-90"
+            data-aos="fade-up"
+            data-aos-duration="600"
+          >
             Explore
           </a>
         </Link>
--- components/homeComponents/HomePage.js
+++ components/homeComponents/HomePage.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Nav from "../globals/Nav";
 import Footer from "../globals/Footer";
 import Hero from "./Hero";
+import AOS from "aos";
+import "aos/dist/aos.css";
 function HomePage() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
     <>
       <Nav />
